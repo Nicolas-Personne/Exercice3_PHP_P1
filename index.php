448fbc7701603b80e8ef00f0874b59dd1f35db69
root <personne.nico@gmail.com>
2018-11-20 15:21:13 +0100

    edit

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<?php
-$km = 1;
-echo $km . '<br>';
-$km = 3;
-echo $km . '<br>';
-$km = 125;
-echo $km . '<br>';
- ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <a href="http://PHP/"><button type="button" class="btn btn-danger" name="button">Accueil</button></a>
+  <?php
+  $km = 1;?>
+  <p><?php echo $km .' '.'km'?></p>
+  <?php
+  $km = 3;
+  ?>
+  <p><?php echo $km.' '.'km'?></p>
+  <?php
+  $km = 125;
+  ?>
+  <p><?php echo $km.' '.'km'?></p>
+</body>
+</html>
